6589813f02621ab9b2e53b7a16e7641b4f378b9e
Vincent Keller <cubisttriangle@gmail.com>
2020-06-05 00:42:39 -0600

    Add main CSS file.

--- blueprints/dashboard/__init__.py
+++ blueprints/dashboard/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
 dashboard = Blueprint( 'dashboard', __name__,
-                       template_folder="templates" )
+                       template_folder='templates' )
 
-@dashboard.route( "/" )
+@dashboard.route( '/' )
 def index():
-    return render_template( "dashboard/index.html" )
+    return render_template( 'dashboard/index.html' )
 

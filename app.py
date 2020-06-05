e05b69d9b68de6f7172c9e81e4e17336a671e0e0
Vincent Keller <cubisttriangle@gmail.com>
2020-06-05 00:23:57 -0600

    Move blueprint code into __init__ so imports are cleaner.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from blueprints.dashboard.dashboard import blueprint as dashboard
+from blueprints.dashboard import dashboard
 
 app = Flask( __name__ )
 app.register_blueprint( dashboard )
